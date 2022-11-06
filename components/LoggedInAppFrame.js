9abeaa316bf1f378ec1d2e4d7b690ca77e6ef1e2
Jack Wang <jackwang@gmail.com>
2022-11-06 01:08:59 -0800

    remove validate in header

--- components/LoggedInAppFrame.js
+++ components/LoggedInAppFrame.js
@@ -14,7 +14,6 @@ const user = {
 const navigation = [
   { name: 'Dashboard', href: '/', current: true },
   { name: 'Credentials', href: '/credentials', current: false },
-  { name: 'Validate', href: '/validate/validate', current: false },
 ]
 const userNavigation = [
   { name: 'Account', href: '/account' },
